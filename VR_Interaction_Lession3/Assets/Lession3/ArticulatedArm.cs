bfd760d973d377f4cbb7beaa1df6c7f4eec2408e
Thor <thor.sone@vobling.com>
2019-10-31 15:30:44 +0100

    Added articulated arm

---
+++ VR_Interaction_Lession3/Assets/Lession3/ArticulatedArm.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArticulatedArm : MonoBehaviour
+{
+    //References to transforms
+    [SerializeField] private Transform link1Transform, link2Transform, headTransform;
+    [SerializeField] private Light light;
+
+//Adds a rotation to Link 1 relative to world
+    public void RotateLink1(float rotationValue) { link1Transform.Rotate(Vector3.up * rotationValue, Space.World); }
+    public void SetLink1Rotation(float rotationValue) { link1Transform.eulerAngles = new Vector3(link1Transform.eulerAngles.x, rotationValue, link1Transform.eulerAngles.z); }
+
+
+}
