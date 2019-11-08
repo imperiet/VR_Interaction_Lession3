44eb63338002c66ae3c8dd426b7c865f17980c78
Thor <thor.sone@vobling.com>
2019-11-08 11:35:05 +0100

    All progress made live coding during lession 6. Added scene HandPanelMenu with a handmounted panel with a lever and canvas. Panel animates on menu button press.

---
+++ VR_Interaction_Lession3/Assets/Lession6/CanvasAlphaController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+
+public class CanvasAlphaController : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public VRTK_BaseControllable controllable;
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        controllable.ValueChanged += OnLeverValueChanged;
+    }
+
+    protected virtual void OnLeverValueChanged(object sender, ControllableEventArgs e)
+    {
+        canvasGroup.alpha = e.value;
+        
+    }
+}
---
+++ VR_Interaction_Lession3/Assets/Lession6/PanelToggle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PanelToggle : MonoBehaviour
+{
+    public UnityEvent OnPanelToggle;
+    public Animator animator;
+    public void TogglePanel(){
+        bool previousValue = animator.GetBool("open");
+        animator.SetBool("open",!previousValue);
+        OnPanelToggle.Invoke();
+    }
+}
